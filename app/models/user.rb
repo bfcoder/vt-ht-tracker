5fbd54a6de6c5ec8b91fe859ae47822d719f4e62
James Carbine <jamescarbine@gmail.com>
2014-11-11 21:19:49 -0700

    Added real roles. Added comments describing how roles work.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # attr_accessible :roles
-
-  ROLES = %i[admin moderator author banned]
+  ROLES = %i[admin presidency teaching_coordinator district_leader visiting_teacher]
 
   def admin?
     self.has_role? :admin
@@ -17,6 +15,14 @@ class User < ActiveRecord::Base
   end
 
   # https://github.com/CanCanCommunity/cancancan/wiki/Role-Based-Authorization#many-roles-per-user
+  # Basically this is done using a bitmask. http://en.wikipedia.org/wiki/Mask_(computing)
+  # There is an integer on the User model.
+  # The integer is turned into a binary number and reversed.
+  # The resulting binary number is a direct translation of the ROLES.
+  # For example: 12 is 1100 in binary,
+  # then reversed is 0011 which gives roles teaching_coordinator and district_leader.
+  # Roles can be set as an array of strings or an array of symbols.
+  # user.roles = [:teaching_coordinator, :district_leader]
   def roles=(roles)
     roles = [*roles].map { |r| r.to_sym }
     self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
