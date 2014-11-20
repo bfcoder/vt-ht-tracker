7aca08044dc4990754488dde03e349b33fda6578
James Carbine <jamescarbine@gmail.com>
2014-11-19 20:23:33 -0700

    Displaying radio buttons for visits

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,9 +1,25 @@
 class DistrictsController < ApplicationController
+
+  respond_to :html
+
   def index
     @districts = District.all
   end
 
   def show
     @district = District.find(params[:id])
+  end
+
+  def update
+    @district = District.find(params[:id])
+    if @district.update_attributes(district_params)
+      redirect_to district_path(@district)
+    else
+      redirect_to district_path(@district), alert: @district.errors.full_messages[0]
+    end
+  end
+
+  def district_params
+    params.require(:district).permit(sisters_attributes: [:id, :name, visits_attributes: [:id, :status]])
   end
 end
