c8fa7b9e20ea6d0ef52a25321239784187034513
James Carbine <jamescarbine@gmail.com>
2014-11-20 18:50:54 -0700

    Adds specs to test visiting teacher access to districts api

--- spec/controllers/api/districts_controller_spec.rb
+++ spec/controllers/api/districts_controller_spec.rb
@@ -83,4 +83,70 @@ RSpec.describe Api::DistrictsController, type: :controller do
     end
   end
 
+  describe 'a logged in visiting_teacher user' do
+    include_context 'a logged in visiting_teacher user'
+
+    describe "GET index" do
+      it "returns all of the requested districts" do
+        @district1 = FactoryGirl.create(:district)
+        @district2 = FactoryGirl.create(:district)
+        @district3 = FactoryGirl.create(:district)
+        get :index, { format: :json, ids: [@district1.id, @district3.id]}
+        result = JSON.parse(response.body)
+        expect(result['districts']).to be_present
+        expect(result['districts'].find{|s| s['id'] == @district1.id}).to be_present
+        expect(result['districts'].find{|s| s['id'] == @district2.id}).to_not be_present
+        expect(result['districts'].find{|s| s['id'] == @district3.id}).to be_present
+      end
+
+      it "returns all of the districts" do
+        @district1 = FactoryGirl.create(:district)
+        @district2 = FactoryGirl.create(:district)
+        @district3 = FactoryGirl.create(:district)
+        get :index, { format: :json }
+        result = JSON.parse(response.body)
+        expect(result['districts']).to be_present
+        expect(result['districts'].find{|s| s['id'] == @district1.id}).to be_present
+        expect(result['districts'].find{|s| s['id'] == @district2.id}).to be_present
+        expect(result['districts'].find{|s| s['id'] == @district3.id}).to be_present
+      end
+    end
+
+    describe "GET show" do
+      it "returns the requested district" do
+        district = FactoryGirl.create(:district)
+        get :show, id: district.to_param, format: :json
+        result = JSON.parse(response.body)
+        expect(result['district']).to be_present
+        expect(result['district']['id']).to eq(district.id)
+      end
+    end
+
+    describe "POST create" do
+      it "returns AccessDenied" do
+        expect {
+          post :create, {district: build_attributes(:district), format: :json}
+        }.to raise_error(CanCan::AccessDenied)
+      end
+    end
+
+    describe "PUT update" do
+      it "returns AccessDenied" do
+        district = FactoryGirl.create(:district)
+        expect {
+          put :update, {id: district.id, district: { "name" => "a name" }, format: :json}
+        }.to raise_error(CanCan::AccessDenied)
+      end
+    end
+
+    describe "DELETE destroy" do
+      it "returns AccessDenied" do
+        district = FactoryGirl.create(:district)
+        expect {
+          delete :destroy, {id: district.to_param, format: :json}
+        }.to raise_error(CanCan::AccessDenied)
+      end
+    end
+  end
+
 end
