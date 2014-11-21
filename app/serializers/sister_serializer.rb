8ee28b02301baaa8a022e2471e9f344064eb2ce7
James Carbine <jamescarbine@gmail.com>
2014-11-21 08:52:14 -0700

    Sister serializer sends current month and previous month visits

--- app/serializers/sister_serializer.rb
+++ app/serializers/sister_serializer.rb
@@ -2,4 +2,8 @@ class SisterSerializer < ActiveModel::Serializer
   attributes :id, :name
 
   has_many :visits
+
+  def visits
+    [object.visits.find_or_create_by(month: Date.today.beginning_of_month - 1.month), object.visits.find_or_create_by(month: Date.today.beginning_of_month)]
+  end
 end
