acb69d52144d93757920ee404123a16f203d8dac
James Carbine <jamescarbine@gmail.com>
2014-11-23 06:20:40 -0700

    Comment out ember logging

--- app/assets/javascripts/districts/application.js
+++ app/assets/javascripts/districts/application.js
@@ -8,12 +8,12 @@
 // for more details see: http://emberjs.com/guides/application/
 VtTracker = Ember.Application.create({
   //Enable logging by uncommenting:
-  LOG_STACKTRACE_ON_DEPRECATION : true,
-  LOG_BINDINGS                  : true,
-  LOG_TRANSITIONS               : true,
-  LOG_TRANSITIONS_INTERNAL      : true,
-  LOG_VIEW_LOOKUPS              : true,
-  LOG_ACTIVE_GENERATION         : true,
+  // LOG_STACKTRACE_ON_DEPRECATION : true,
+  // LOG_BINDINGS                  : true,
+  // LOG_TRANSITIONS               : true,
+  // LOG_TRANSITIONS_INTERNAL      : true,
+  // LOG_VIEW_LOOKUPS              : true,
+  // LOG_ACTIVE_GENERATION         : true,
 
   rootElement: "#EmberStart"
 });
