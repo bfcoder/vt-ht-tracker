17008555f763f2a4092d8cde45918029687a020e
James Carbine <jamescarbine@gmail.com>
2014-11-23 06:37:17 -0700

    Removed pending specs

--- spec/helpers/districts_helper_spec.rb
+++ spec/helpers/districts_helper_spec.rb
@@ -10,6 +10,5 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe DistrictsHelper, :type => :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe DistrictsHelper, type: :helper do
 end
