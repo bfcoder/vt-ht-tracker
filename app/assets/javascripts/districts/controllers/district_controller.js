fe0a62a9ccace36ec356497e15f9840a6f3f2277
James Carbine <jamescarbine@gmail.com>
2014-11-23 07:32:33 -0700

    Displaying checkmark indicating the visits were saved.

--- app/assets/javascripts/districts/controllers/district_controller.js
+++ app/assets/javascripts/districts/controllers/district_controller.js
@@ -1,11 +1,29 @@
 VtTracker.DistrictController = Ember.ObjectController.extend({
+  isSaving: false,
+
+  showSaveNotice: function() {
+    return this.get('isSaving');
+  }.property('isSaving'),
+
+  enableSaveNotice: function() {
+    this.set('isSaving', true);
+  },
+
+  disableSaveNotice: function() {
+    this.set('isSaving', false);
+  },
+
   actions: {
     saveVisits: function() {
-      this.get('sisters').forEach(function(sister) {
+      var _self = this;
+      _self.get('sisters').forEach(function(sister) {
         sister.get('visits').forEach(function(visit) {
           // If the status has chaged, then save that visit.
           if (visit.get('isDirty')) {
-            return visit.save();
+            return visit.save().then(function() {
+              _self.enableSaveNotice();
+              Ember.run.debounce(_self, _self.disableSaveNotice, 2000);
+            });
           }
         });
       });
