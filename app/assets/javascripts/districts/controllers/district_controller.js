b294ef56f8b16e99cbe09a6fae1aac5bab0797a0
James Carbine <jamescarbine@gmail.com>
2014-11-23 06:19:42 -0700

    Saving visits

--- app/assets/javascripts/districts/controllers/district_controller.js
+++ app/assets/javascripts/districts/controllers/district_controller.js
@@ -1,1 +1,14 @@
-VtTracker.DistrictController = Ember.ObjectController.extend({});
+VtTracker.DistrictController = Ember.ObjectController.extend({
+  actions: {
+    saveVisits: function() {
+      this.get('sisters').forEach(function(sister) {
+        sister.get('visits').forEach(function(visit) {
+          // If the status has chaged, then save that visit.
+          if (visit.get('isDirty')) {
+            return visit.save();
+          }
+        });
+      });
+    }
+  }
+});
