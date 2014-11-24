20a37753b506a40f95f0db10d7718cb0d5c91ac2
James Carbine <jamescarbine@gmail.com>
2014-11-23 22:40:03 -0700

    Only displaying other selection for a visit to authorized users.

--- app/assets/javascripts/districts/models/user.js
+++ app/assets/javascripts/districts/models/user.js
@@ -7,6 +7,26 @@ VtTracker.User = DS.Model.extend({
     return this.hasRole('admin');
   }.property('roles.@each'),
 
+  isPresidency: function(){
+    return this.hasRole('presidency');
+  }.property('roles.@each'),
+
+  isTeachingCoordinator: function(){
+    return this.hasRole('teaching_coordinator');
+  }.property('roles.@each'),
+
+  isDistrictLeader: function(){
+    return this.hasRole('district_leader');
+  }.property('roles.@each'),
+
+  isVisitingTeacher: function(){
+    return this.hasRole('visiting_teacher');
+  }.property('roles.@each'),
+
+  canSetVisitsToOther: function(){
+    return this.get('isAdmin') || this.get('isPresidency') || this.get('isTeachingCoordinator') || this.get('isDistrictLeader');
+  }.property('isAdmin', 'isPresidency', 'isTeachingCoordinator', 'isDistrictLeader'),
+
   hasRole: function(role_name){
     var roles = this.get('roles');
     if(!Ember.isEmpty(roles)){
