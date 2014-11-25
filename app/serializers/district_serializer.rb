efb495f3e7254464ac49e177a0b624c3a51769e0
James Carbine <jamescarbine@gmail.com>
2014-11-24 22:12:36 -0700

    Sorting sisters by last name in the district serializer

--- app/serializers/district_serializer.rb
+++ app/serializers/district_serializer.rb
@@ -2,4 +2,8 @@ class DistrictSerializer < ActiveModel::Serializer
   attributes :id, :name
 
   has_many :sisters
+
+  def sisters
+    object.sisters.sort_by {|sister| sister.last_name}
+  end
 end
