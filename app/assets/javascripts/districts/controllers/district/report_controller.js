020d25d881b1fd6b72c7e1b5171d296719c332c5
James Carbine <jamescarbine@gmail.com>
2014-11-27 19:33:50 -0700

    Displaying month names on report

--- app/assets/javascripts/districts/controllers/district/report_controller.js
+++ app/assets/javascripts/districts/controllers/district/report_controller.js
@@ -3,4 +3,12 @@ VtTracker.DistrictReportController = Ember.ObjectController.extend({
 
   sistersSorting: ['lastName'],
   sortedSisters: Ember.computed.sort('sisters', 'sistersSorting'),
+
+  previousMonth: function() {
+    return moment(new Date()).subtract(1, 'months').format('MMMM');
+  }.property(),
+
+  currentMonth: function() {
+    return moment(new Date()).format('MMMM');
+  }.property()
 });
