37d9652d196e2c99901747201152082b02dff560
James Carbine <jamescarbine@gmail.com>
2014-11-30 17:42:21 -0700

    Protecting Report route from unauthorized access

--- app/assets/javascripts/districts/routes/district/report_route.js
+++ app/assets/javascripts/districts/routes/district/report_route.js
@@ -1,4 +1,11 @@
 VtTracker.DistrictReportRoute = Ember.Route.extend({
+  beforeModel: function(transition){
+    if (!this.get('currentUser.isPrivileged')){
+      transition.abort();
+      this.transitionTo('/');
+    }
+  },
+
   model: function(){
     return this.modelFor('district');
   }
