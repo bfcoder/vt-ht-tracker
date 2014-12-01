428a3197578c478916e9e2bd9732df5c8e18523f
James Carbine <jamescarbine@gmail.com>
2014-11-30 17:42:41 -0700

    Protecting Sisters Index route from unauthorized access

--- app/assets/javascripts/districts/routes/district/sisters/index_route.js
+++ app/assets/javascripts/districts/routes/district/sisters/index_route.js
@@ -1,7 +1,15 @@
 VtTracker.DistrictSistersIndexRoute = Ember.Route.extend({
+  beforeModel: function(transition){
+    if (!this.get('currentUser.isPrivileged')){
+      transition.abort();
+      this.transitionTo('/');
+    }
+  },
+
   model: function(){
     return this.modelFor('district');
   },
+
   setupController: function(controller, district) {
     this._super(controller, district);
 
