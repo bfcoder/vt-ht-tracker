a83515ecd2b9070b647759b8bb2cc296c7c610a2
James Carbine <jamescarbine@gmail.com>
2014-12-15 23:01:30 -0700

    Displaying visited percent on each individual district report

--- app/assets/javascripts/districts/controllers/district/report_controller.js
+++ app/assets/javascripts/districts/controllers/district/report_controller.js
@@ -2,5 +2,38 @@ VtTracker.DistrictReportController = Ember.ObjectController.extend(VtTracker.Com
   breadCrumb: 'Report',
 
   sistersSorting: ['lastName'],
-  sortedSisters: Ember.computed.sort('sisters', 'sistersSorting')
+  sortedSisters: Ember.computed.sort('sisters', 'sistersSorting'),
+
+  percents: function() {
+    var num_visited = {
+      previous_month: 0,
+      current_month: 0,
+      number_sisters: 0
+    };
+
+    var district_percent_visited = this.get('model.percentVisited');
+    num_visited.previous_month += district_percent_visited.previous_month;
+    num_visited.current_month += district_percent_visited.current_month;
+    num_visited.number_sisters += district_percent_visited.number_sisters;
+
+    var computed_percent_visited = {
+      previous_month: 0,
+      current_month: 0
+    };
+
+    if (num_visited.number_sisters > 0) {
+      computed_percent_visited.previous_month = num_visited.previous_month / num_visited.number_sisters;
+      computed_percent_visited.current_month = num_visited.current_month / num_visited.number_sisters;
+    }
+
+    return computed_percent_visited;
+  }.property('model.percentVisited'),
+
+  previousMonthPercentVisited: function() {
+    return parseInt(this.get('percents').previous_month * 100, 10);
+  }.property('percents'),
+
+  currentMonthPercentVisited: function() {
+    return parseInt(this.get('percents').current_month * 100, 10);
+  }.property('percents')
 });
