232cb1b3f8f597ff6e100f42a88ae064f0d03fbf
James Carbine <jamescarbine@gmail.com>
2015-06-13 16:33:26 -0600

    Fix sister factory

--- spec/factories/sisters.rb
+++ spec/factories/sisters.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :sister do
-    name { FactoryGirl.generate(:name) }
+    first_name { FactoryGirl.generate(:name) }
+    last_name { FactoryGirl.generate(:name) }
     district
   end
 
