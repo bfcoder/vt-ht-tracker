3aeb8deead205399ddaf864662cc9629fe22879d
James Carbine <jamescarbine@gmail.com>
2015-06-13 20:42:17 -0600

    Adds Settings page

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,7 +1,21 @@
 class SettingsController < ApplicationController
+  before_filter :authenticate_user!
+  load_and_authorize_resource
+
   def show
   end
 
   def update
+    if @setting.update_attributes(setting_params)
+      redirect_to setting_path, notice: "Settings updated."
+    else
+      redirect_to setting_path, alert: "Unable to update settings."
+    end
   end
+
+  private
+
+    def setting_params
+      params.require(:setting).permit(:mode)
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def brand_title
+    @setting ||= Setting.first
+    if @setting.mode == 'visiting_teaching'
+      'Visiting Teaching Tracker'
+    elsif @setting.mode == 'home_teaching'
+      'Home Teaching Tracker'
+    end
+  end
 end
