d56b93b3b689aea6b37194b3bc3e3cc9a7d1511b
James Carbine <jamescarbine@gmail.com>
2015-08-30 18:37:23 -0600

    Adds csrf token to header of requests

--- tracker-client/app/adapters/application.js
+++ tracker-client/app/adapters/application.js
@@ -2,9 +2,14 @@
 
 import Ember from "ember";
 import DS    from "ember-data";
+import $     from "jquery";
 
 export default DS.ActiveModelAdapter.extend({
   namespace: "api",
+
+  headers: {
+    "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
+  },
 
   shouldReloadAll: function() {
     return true;
