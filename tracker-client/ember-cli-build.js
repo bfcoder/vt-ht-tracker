89488cced3746c3c64176c04c83046f9201074c8
James Carbine <jamescarbine@gmail.com>
2015-09-01 06:42:06 -0600

    Don’t minify ember stuff Add source map to mime type as application/json

--- tracker-client/ember-cli-build.js
+++ tracker-client/ember-cli-build.js
@@ -4,6 +4,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    minifyCSS: {
+      enabled: false
+    },
+    minifyJS: {
+      enabled: false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
