cde960b10ebf7dd027a50d0d914506715ed841c8
James Carbine <jamescarbine@gmail.com>
2015-09-04 06:03:30 -0600

    Creates a visit history every time a visit is saved

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,12 @@
 class Visit < ActiveRecord::Base
   belongs_to :sister
   belongs_to :household
+  has_many :histories, dependent: :destroy
+
+  after_save :create_history
+
+  protected
+    def create_history
+      self.histories.create(month: self.month, status: self.status, notes: self.notes)
+    end
 end
--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Visit, type: :model do
+  describe "Visit Histories" do
+    before do
+      @visit = FactoryGirl.create(:visit)
+    end
+
+    it "creates a visit history when saved" do
+      @visit.month = FactoryGirl.generate(:month)
+      @visit.status = FactoryGirl.generate(:status)
+      @visit.notes = FactoryGirl.generate(:notes)
+      @visit.save
+      expect(@visit.histories.count).to eq(2)
+      expect(@visit.histories.last.month).to eq(@visit.month)
+      expect(@visit.histories.last.status).to eq(@visit.status)
+      expect(@visit.histories.last.notes).to eq(@visit.notes)
+    end
+  end
 end
