8217adedb7de5eedfd00fdc0256d903d0f05359d
James Carbine <jamescarbine@gmail.com>
2015-09-05 19:08:18 -0600

    Updates visit model Only create a history if it isn’t a new record.

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,13 +3,16 @@ class Visit < ActiveRecord::Base
   belongs_to :household
   has_many :histories, dependent: :destroy
 
-  after_save :create_history
+  before_save :create_history
+
   scope :with_content, -> { where.not(status: '') }
   scope :without_content, -> { where(status: nil) }
   scope :without_history, -> { includes(:histories).where(histories: { visit_id: nil }) }
 
   protected
     def create_history
-      self.histories.create(month: self.month, status: self.status, notes: self.notes)
+      unless self.new_record?
+        self.histories.create(month: self.month, status: self.status, notes: self.notes)
+      end
     end
 end
