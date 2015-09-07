83047305cdfa06905342e3f47ec428f6f33648d6
James Carbine <jamescarbine@gmail.com>
2015-09-07 09:57:50 -0600

    Fixes sign out error

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   root 'districts#index'
 
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :users
   resources :districts
   resources :settings, only: [:show, :update]
