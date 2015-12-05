bc1565b7de8186875d679ec071b0b9b79727f458
James Carbine <jamescarbine@gmail.com>
2015-12-05 10:54:44 -0700

    Show disabled households on all households page

--- tracker-client/app/controllers/home/households/index.js
+++ tracker-client/app/controllers/home/households/index.js
@@ -6,10 +6,30 @@ export default Ember.Controller.extend({
   householdsSorting: ['district.name', 'teachers', 'name'],
   sortedHouseholds: Ember.computed.sort('model', 'householdsSorting'),
 
+  activeHouseholds: Ember.computed.filter('model.@each.status', function(household) {
+    return household.get('status');
+  }),
+  deactiveHouseholds: Ember.computed.filter('model.@each.status', function(household) {
+    return !household.get('status');
+  }),
+
+  sortedActiveHouseholds: Ember.computed.sort('activeHouseholds.@each.status', 'householdsSorting'),
+  sortedDeactiveHouseholds: Ember.computed.sort('deactiveHouseholds.@each.status', 'householdsSorting'),
+
   actions: {
     createNewHousehold: function() {
       var newHousehold = this.store.createRecord('household');
       this.send('showModal', 'household-modal', newHousehold);
+    },
+
+    disableHousehold: function(household) {
+      household.set('status', false);
+      household.save();
+    },
+
+    enableHousehold: function(household) {
+      household.set('status', true);
+      household.save();
     }
   }
 });
