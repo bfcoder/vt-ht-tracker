78cb4ed2a13c1e52e63ba29b01f26806dd159c81
James Carbine <jamescarbine@gmail.com>
2015-12-05 10:12:44 -0700

    Show disabled sisters in district sisters page

--- tracker-client/app/controllers/district/sisters/index.js
+++ tracker-client/app/controllers/district/sisters/index.js
@@ -4,7 +4,16 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
   sistersSorting: ['teachers', 'lastName', 'firstName'],
-  sortedSisters: Ember.computed.sort('model.sisters', 'sistersSorting'),
+
+  activeSisters: Ember.computed.filter('model.sisters.@each.status', function(sister) {
+    return sister.get('status');
+  }),
+  deactiveSisters: Ember.computed.filter('model.sisters.@each.status', function(sister) {
+    return !sister.get('status');
+  }),
+
+  sortedActiveSisters: Ember.computed.sort('activeSisters.@each.status', 'sistersSorting'),
+  sortedDeactiveSisters: Ember.computed.sort('deactiveSisters.@each.status', 'sistersSorting'),
 
   actions: {
     createNewSister: function() {
@@ -12,6 +21,16 @@ export default Ember.Controller.extend({
         district: this.get('model')
       });
       this.send('showModal', 'sister-modal', newSister);
+    },
+
+    disableSister: function(sister) {
+      sister.set('status', false);
+      sister.save();
+    },
+
+    enableSister: function(sister) {
+      sister.set('status', true);
+      sister.save();
     }
   }
 });
