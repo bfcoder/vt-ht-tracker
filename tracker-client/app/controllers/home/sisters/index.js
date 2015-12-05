7101bbb7c698a0e87a1480ae26bbc868f1044317
James Carbine <jamescarbine@gmail.com>
2015-12-05 10:50:05 -0700

    Show disabled sisters on all sisters page

--- tracker-client/app/controllers/home/sisters/index.js
+++ tracker-client/app/controllers/home/sisters/index.js
@@ -4,12 +4,32 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
   sistersSorting: ['district.name', 'teachers', 'lastName', 'firstName'],
-  sortedSisters: Ember.computed.sort('model', 'sistersSorting'),
+
+  activeSisters: Ember.computed.filter('model.@each.status', function(sister) {
+    return sister.get('status');
+  }),
+  deactiveSisters: Ember.computed.filter('model.@each.status', function(sister) {
+    return !sister.get('status');
+  }),
+
+  sortedActiveSisters: Ember.computed.sort('activeSisters.@each.status', 'sistersSorting'),
+  sortedDeactiveSisters: Ember.computed.sort('deactiveSisters.@each.status', 'sistersSorting'),
+
 
   actions: {
     createNewSister: function() {
       var newSister = this.store.createRecord('sister');
       this.send('showModal', 'sister-modal', newSister);
+    },
+
+    disableSister: function(sister) {
+      sister.set('status', false);
+      sister.save();
+    },
+
+    enableSister: function(sister) {
+      sister.set('status', true);
+      sister.save();
     }
   }
 });
